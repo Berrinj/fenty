8c347cf5ebeb9a3afae0758844a4f8d9c3f5f449
Berrinj <69366646+Berrinj@users.noreply.github.com>
2025-10-09 17:51:16 +0200

    added fxp and updated form id

--- js/components/filterPosts.js
+++ js/components/filterPosts.js
@@ -1,33 +1,35 @@
 let selectHeading = document.querySelector(".news-and-content");
 
-export function sortPosts(selectedValue) {    
-
-    switch (selectedValue) {
-        case "All":
-            selectHeading.textContent = "ALLE INNLEGG";
-            break;
-        case "9":
-            selectHeading.textContent = "FENTY BEAUTY";
-            break;
-        case "10":
-            selectHeading.textContent = "FENTY SKIN";
-            break;
-        case "15":
-            selectHeading.textContent = "SAVAGE X";
-            break;
-        case "12":
-            selectHeading.textContent = "MUSIKK";
-            break;
-        case "14":
-            selectHeading.textContent = "SAMARBEID";
-            break;
-        case "13":
-            selectHeading.textContent = "PRISUTDELINGER";
-            break;
-        case "1":
-            selectHeading.textContent = "UKATEGORISERT";
-            break;
-        default:
-            selectHeading.textContent = "Alle innlegg";
-    }
+export function sortPosts(selectedValue) {
+  switch (selectedValue) {
+    case "All":
+      selectHeading.textContent = "ALLE INNLEGG";
+      break;
+    case "4":
+      selectHeading.textContent = "FENTY BEAUTY";
+      break;
+    case "3":
+      selectHeading.textContent = "FENTY SKIN";
+      break;
+    case "5":
+      selectHeading.textContent = "SAVAGE X";
+      break;
+    case "7":
+      selectHeading.textContent = "MUSIKK";
+      break;
+    case "6":
+      selectHeading.textContent = "SAMARBEID";
+      break;
+    case "8":
+      selectHeading.textContent = "PRISUTDELINGER";
+      break;
+    case "9":
+      selectHeading.textContent = "FENTY X PUMA";
+      break;
+    case "1":
+      selectHeading.textContent = "UKATEGORISERT";
+      break;
+    default:
+      selectHeading.textContent = "Alle innlegg";
+  }
 }
