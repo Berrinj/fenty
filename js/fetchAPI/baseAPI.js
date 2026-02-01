c162829e358cd7fbdddbb0d64caae5a0887c0510
Marte Lilleberre <martelilleberre@gmail.com>
2026-02-01 11:32:18 +0100

    updating base url

--- js/fetchAPI/baseAPI.js
+++ js/fetchAPI/baseAPI.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = `https://fenty.no/wp-json/wp/v2`;
+export const API_BASE_URL = `https://wp.fenty.no/wp-json/wp/v2`;
 
 export const FENTY_API_URL = `${API_BASE_URL}/posts`;
